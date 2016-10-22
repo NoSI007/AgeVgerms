eab5c8abf70ff92a02a0f8edfa332b3611f15815
Khaled Alwan <khaled_alwan@hotmail.com>
2016-10-23 00:06:11 +0100

    uploaded project files.

---
+++ germsVage/germsVage/Query.xaml.cs
@@ -0,0 +1,43 @@
+﻿// (c) Khaled A Alwan .
+// All other rights reserved.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace germsVage
+{
+    /// <summary>
+    /// Interaction logic for Query.xaml
+    /// </summary>
+    public partial class Query : Window
+    {
+        public Query()
+        {
+            InitializeComponent();
+        }
+
+        private void Calc_Click(object sender, RoutedEventArgs e)
+        {
+            double gr;
+            gVaitem gv = new gVaitem();
+            if(string.IsNullOrWhiteSpace(zgr.Text) == false)
+            {
+                if( double.TryParse(zgr.Text, out gr) )
+                {
+                    gv.setAge(gr);
+                    MessageBox.Show(gv.Age.ToString());
+                }
+            }
+        }
+    }
+}
